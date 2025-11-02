a66640cbb0643db15a8ab920af34378db460557f
JohnDeeZimmermann <johndee.zimmermann@gmail.com>
2025-11-02 20:54:59 +0100

    Simplify harpoon settings

--- nvim/lua/harpoon_settings.lua
+++ nvim/lua/harpoon_settings.lua
@@ -2,30 +2,24 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 -- Retrieving harpoons
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
-vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
-vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
-vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end)
+local shift_buttons = "!@#$%^&*()"
+for i = 1, 8 do
+    vim.keymap.set("n", "<C-" .. i .. ">", function() harpoon:list():select(i) end)
+    vim.keymap.set("n", "<leader>h" .. i, function() harpoon:list():select(i) end)
 
--- Harpooning at specific index
-vim.keymap.set("n", "<C-!>", function() harpoon:list():replace_at(1) end)
-vim.keymap.set("n", "<C-@>", function() harpoon:list():replace_at(2) end)
-vim.keymap.set("n", "<C-#>", function() harpoon:list():replace_at(3) end)
-vim.keymap.set("n", "<C-$>", function() harpoon:list():replace_at(4) end)
-vim.keymap.set("n", "<C-%>", function() harpoon:list():replace_at(5) end)
-vim.keymap.set("n", "<C-^>", function() harpoon:list():replace_at(6) end)
-vim.keymap.set("n", "<C-&>", function() harpoon:list():replace_at(7) end)
-vim.keymap.set("n", "<C-*>", function() harpoon:list():replace_at(8) end)
+    vim.keymap.set("n", "<C-" .. shift_buttons:sub(i,i) .. ">", function()
+        harpoon:list():replace_at(i)
+    end)
+    vim.keymap.set("n", "<leader>hs" .. i, function()
+        harpoon:list():replace_at(i)
+    end)
+end
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
+vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
